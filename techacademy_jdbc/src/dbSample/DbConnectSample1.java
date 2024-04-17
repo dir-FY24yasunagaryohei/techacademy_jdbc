05dcdde5fa03f40245e9c97e3a35c494e909bfcc
ryrydirbato <ryohei.yasunaga.d819@dirbato.co.jp>
2024-04-17 14:13:08 +0900

    最初(？)のコミット

--- techacademy_jdbc/src/dbSample/DbConnectSample1.java
+++ techacademy_jdbc/src/dbSample/DbConnectSample1.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 
-public class DbConnectSample01 {
+public class DbConnectSample1 {
 
     public static void main(String[] args) {
         
